b46c01896aeaa59c52ab4126b993d589dcc26747
m-salikhov <andvary@inbox.ru>
2021-03-04 18:45:52 +0300

    init commit

---
+++ Module21_homework/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/scripts/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.pug',
+      filename: 'index.html',
+    }),
+    new TerserWebpackPlugin(),
+    new OptimizeCssAssetsWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin(),
+      new OptimizeCssAssetsWebpackPlugin(),
+    ],
+  },
+  devServer: {
+    port: 4200,
+  },
+};
